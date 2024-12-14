4fb50291319c85b6f6cd908103b0e674046bf8b3
MrBigPorter <mrsuperporter@gmail.com>
2024-12-14 12:50:45 +0800

    add swicth

--- components/common/explore/ExploreList.tsx
+++ components/common/explore/ExploreList.tsx
@@ -1,7 +1,9 @@
 import ExploreItem from './ExploreItem';
 import { useState } from 'react';
 import { ExploreItemType, ExploreListProps } from '@/types/exploreTypes';
-import { FlatList, NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
+import { FlatList, NativeScrollEvent, NativeSyntheticEvent, View, Text, StyleSheet } from 'react-native';
+import { useCustomTheme } from '@/context/themeContext';
+import { Switch } from 'react-native-paper';
 
 export default function ExploreList({onScroll}:ExploreListProps) {
   const [list, setList] = useState<ExploreItemType[]>([
@@ -58,20 +60,82 @@ export default function ExploreList({onScroll}:ExploreListProps) {
       ],
     },
   ]);
+  const { theme:{border, background} } = useCustomTheme();
+
+  const [switchValue, setSwitchValue] = useState<boolean>(false);
 
   // 处理滚动事件 Handle scroll events
   const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     onScroll && onScroll(event.nativeEvent.contentOffset.y);
   };
 
+  const ListHeaderComponent = () => {
+    return (
+      <View style={styles.listHeader}>
+        <View style={[styles.listHeaderButtonBox, {borderColor: border.default}]}>
+          <View style={styles.listHeaderButtonBoxLeft}>
+            <Text>Display total price</Text>
+            <Text>Includes all fees, before taxes</Text>
+          </View>
+          <View style={styles.listHeaderButtonBoxRight}>
+            <Switch
+              thumbColor={background.default}
+              value={switchValue}
+              onValueChange={() => {
+                setSwitchValue(!switchValue);
+              }}
+              color={border.default}
+              
+           />
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <FlatList 
-      data={list}
-      renderItem={({ item }) => <ExploreItem key={item.id} item={item} />}
-      keyExtractor={(item) => item.id.toString()}
-      onScroll={handleScroll}
-      scrollEventThrottle={16}
-      showsVerticalScrollIndicator={false}
-    />
+    <View style={{ flex: 1 }}>
+      <FlatList 
+        ListHeaderComponent={ListHeaderComponent}
+        data={list}
+        renderItem={({ item }) => <ExploreItem key={item.id} item={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  listHeader: {
+    padding: 24,
+    boxSizing: 'border-box',
+  },
+  listHeaderButtonBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 10,
+    height: 64,
+    padding:10
+  },
+  listHeaderButtonBoxLeft: {
+    flex: 1,
+  },
+  listHeaderButtonBoxRight: {
+    width: 48,
+  },
+  listHeaderSwitchBox: {
+    width: 48,
+  },
+  listHeaderSwitchActive: {
+    backgroundColor: 'red',
+  },
+  listHeaderSwitchInactive: {
+    color: 'blue',
+  },
+}); 
